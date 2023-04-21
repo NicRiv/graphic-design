051abf4f87a2003b0f1d07afe5984f63f45a3a7f
Nicolás Rivera <nicriv.dev@gmail.com>
2023-04-21 16:59:57 -0300

    init page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,7 @@
 <template>
-  <div class="about">
-    <!-- <h1>This is an about page</h1> -->
-    <p class="text-xl text-red-300">Tailwind test</p>
+  <div class="">
+
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+
